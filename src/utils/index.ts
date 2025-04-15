60c414b41c6bd3bd744b024838fcb079005c83de
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2025-04-15 22:57:16 +0200

    Text Slicer Function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
+/**
+ *
+ * @param (string) number -The price without separators
+ * @returns (string) number -The price after it is separated by comma each 3 digits
+ */
 export function priceSeparator(price: string): string {
   return price.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+/**
+ * @param (string) text -Input text that need to be truncated.
+ * @param (number) max -The maximum number of charachters needed.
+ * @returns (string) text -The text after being truncated.
+ */
+export function sliceText(description: string, max: number): string {
+  return description.length > max
+    ? description.slice(0, max) + "..."
+    : description;
 }
